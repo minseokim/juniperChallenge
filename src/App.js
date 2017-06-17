c54975713daeb708cd338ff082406e80f912a7a6
minseokim <minseoalexkim@gmail.com>
2017-06-17 13:49:31 -0400

    Create static version of App component that renders UserForm, QueryDisplay components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import UserForm from "./components/UserForm";
+import QueryDisplay from "./components/QueryDisplay";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App__header">
+          Query Generator
+        </header>
+        <section className="container-fluid App__container">
+          <div className="row">
+            <div className="col-sm-6">
+              <UserForm />
+            </div>
+            <div className="col-sm-6">
+              <QueryDisplay />
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
